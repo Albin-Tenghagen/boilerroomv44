4ab6326a3fda93f60ea1c8996ab3c6cf4bfd18f6
Zana Palani <timezana10@gmail.com>
2024-11-03 19:14:36 +0100

    fixed remove task function

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ function addNewObj(id, description, finished){
         // myArray.push({ id, description, finished }); // changes the original array rather than creating a new one.
         
         const taskContainer = document.getElementById("taskItems");
-        // taskContainer.innerHTML = ''; 
+        taskContainer.innerHTML = ''; 
         
        
         for (let i = 0; i < myArray.length; i++) {
@@ -100,10 +100,72 @@ saveAndDisplayTasks();
 
 // removeTask(){
 
-myArray.splice(1, 1,);
-console.log(myArray); 
+
+// function removeTask(){
+//     let userRemoveTask = prompt('Pick a task to take of the list, use a number please')
+
+//     if userRemoveTask == myArray[i] {
+//         myArray.splice()
+//     }
+//     myArray.splice(1, 1,);
+//     console.log(myArray); 
+// }
 
 
+//  function removeTask(){
+//     let removeByIndex = prompt("Enter the index you wish to remove: ");
+//     let index = myArray.indexOf(removeByIndex);
+//     if(index > -1){ // HERE!!!
+//         myArray.splice(index, 1);
+
+//     }
+//     else{
+//         alert("id is not in the system");
+//     }
+// }
+
+// let remove = function() {
+//     let removeID = parseInt(prompt("enter index of array to remove"));
+//     let index = myArray.indexOf(removeID);
+//     if (index > -1){
+//         myArray.splice(index, 1);
+//         showArray();
+//     }
+//     else{
+//         alert("index not found")
+//     }
+// }
+// remove();
+
+
+// function removeTaskById() {
+//     const taskId = prompt("Enter ID:");
+//     if (!taskId) {
+//         console.log("No ID entred.");
+//         return;
+//     }
+//     const index = myArray.indexOf(task => task.id === taskId);
+//     if (index !== -1) {
+//         myArray.splice(index, 1); // 
+//         console.log(`id "${taskId}"  removed.`);
+//         saveAndDisplayTasks(); 
+//     } else {
+//         console.log(`id "${taskId}" not found.`);
+//     }
+// }
+
+
+function removeTaskById() {
+    const indexInput = prompt("Enter index:");
+    const index = parseInt(indexInput, 10); 
+
+    myArray.splice(index, 1); // 
+    console.log(`Task at index ${index} removed.`);
+    saveAndDisplayTasks(); 
+}
+
+removeTaskById();
+console.log(myArray);
 
 
 // använd splice för o ta bort.
