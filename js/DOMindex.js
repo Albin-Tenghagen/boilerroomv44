a00a5b9a1d4859a72b6270bf8622e4aa0eec523b
Albin Tenghagen <albin.tenghagen@chasacademy.se>
2024-11-08 14:22:08 +0100

    I linked the list item to create an object that gets pushed into an array, a button has also been added as a child element to the list item. that should then be linked to finishing a task. putting it in another list called finished tasks

--- js/DOMindex.js
+++ js/DOMindex.js
@@ -1,8 +1,17 @@
 let taskButton = document.getElementById("addTaskBtn")
+taskButton.setAttribute("id", "taskButton")
 let inputFieldTask = document.getElementById("addTask")
+inputFieldTask.setAttribute("id", "inputFieldTask")
+
+let taskArray = []
+let taskObject = {
+    description: " ",
+    finished: undefined
+};
 
 //
 let taskListContainer = document.createElement("main")
+taskListContainer.setAttribute("id", "taskListContainer")
 document.body.appendChild(taskListContainer)
 
 let containerHeadLine = document.createElement("h3")
@@ -16,7 +25,25 @@ taskListContainer.appendChild(taskList)
 
 taskButton.addEventListener("click", function(){
     let listItem = document.createElement("li")
+    listItem.setAttribute("class", "listItem")
     listItem.innerText = inputFieldTask.value;
     taskList.appendChild(listItem)
-    console.log(listItem)
+    
+    let finishButton = document.createElement("button")
+    finishButton.innerHTML = "Finish Task"
+    finishButton.setAttribute("id", "finishbutton")
+    listItem.appendChild(finishButton)
+    
+    objectCreation()
 })
+
+//*Function that creates an object in taskArray basen on taskbutton.Eventlistener function
+function objectCreation(){
+    let newObject = Object.create(taskObject);
+    newObject.description = inputFieldTask.value;
+    newObject.finished = undefined;
+    taskArray.push(newObject);
+    console.log(taskArray)
+    return;
+}
+//*-----------------------------------------------------------
